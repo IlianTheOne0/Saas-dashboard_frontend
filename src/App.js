0afc459743fa68728a1455d41185320327a48dab
Shchepinskyi Ilian Oleksandrovych <ilian.dev.ua@gmail.com>
2025-11-21 23:58:57 +0200

    Revert "Features/kafka"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import { KafkaProvider } from "./context/kafka.context";
-
-import KafkaServiceTest from "./features/tests/TestKafka";
-
 function App()
 {
-	return (
-		<KafkaProvider>
-			{/*Test Components*/}
-			{/* <div>
-				<KafkaServiceTest/>
-			</div> */}
-		</KafkaProvider>
-	);
+	
 }
 
 export default App;
